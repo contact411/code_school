7e85e80c377f6e460c7f2718321a6ef61d6da380
Joel Hampton <joelhampton@gmail.com>
2015-04-23 22:02:37 +1000

    Complete track 1

---
+++ javascript/notes.js
@@ -0,0 +1,11 @@
+// PEMDAS
+(5 + 7) * 3
+
+
+var numPassengers = 0
+numPassengers += 3
+
+var trainCapacity = 60
+numPassengers--
+trainCapacity -= numPassengers
+var occupancy = numPassengers + " passengers present, with " + trainCapacity + " seats remaining."
